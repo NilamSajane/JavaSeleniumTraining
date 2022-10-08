c9c34087a513dbe47132ade109f634a6c3e34877
unknown <nilamsajane2761@gmail.com>
2022-10-08 06:30:46 +0530

    Adding my ALL files

---
+++ src/test/java/day1/JavaDataTypes.java
@@ -0,0 +1,31 @@
+package day1;
+
+/**
+ * @auther arvind
+ * @Description This is basic java class
+ */
+public class JavaDataTypes {
+
+    public static void main(String[] args) {
+
+        int age =30;
+        //System.out.println(Age: '+age);
+        short pin = 12344;
+        Long aadhar=123412341234l;
+        float weight =20.111f;
+        double rate =1341.1324;
+        char ch ='c';
+        boolean wfh = false;
+
+        System.out.println("pin: "+ pin);
+
+       /*
+       This
+       is multi line comment
+        */
+
+    }
+
+
+
+}
