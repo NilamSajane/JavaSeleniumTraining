7cef2ad9e864403362df73643891de36beb5852a
Nilam <nilamsajane2761@gmail.com>
2022-11-24 09:57:21 +0530

    adding my file

---
+++ src/test/java/Assignment2/ConcateString.java
@@ -0,0 +1,11 @@
+package Assignment2;
+
+public class ConcateString {
+
+    public static void main(String[] args) {
+        String str1 = "PHP Exercises and";
+        String str2 = " Python Exercises";
+
+        System.out.println(str1.concat (str2));
+    }
+}
