c8e4bc8049f0bc472673c81c76364a85f3af1a1a
Nilam <nilamsajane2761@gmail.com>
2022-11-24 09:07:18 +0530

    adding my file

---
+++ src/test/java/Assignment2/Character.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+public class Character {
+
+    public static void main(String[] args) {
+                       //0,1,2,3,4,5,6,7,8,9,10
+        String name = "Java Exercises!";
+
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(10));
+    }
+}
